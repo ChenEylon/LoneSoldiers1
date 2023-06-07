db6a7faa6a32219f6ae49f60135d85e99557df3b
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-06-07 21:55:48 +0300

    login fix

--- src/components/LoginSoldier.jsx
+++ src/components/LoginSoldier.jsx
@@ -1,79 +1,86 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginSoldier() {
+  const [usersArr, setUsersArr] = useState(
+    JSON.parse(localStorage.getItem("loneSoldierArr")) || []
+  );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginData, setLoginData] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+  const [theUser, setTheUser] = useState(null);
 
-    const [usersArr, setUsersArr] = useState(JSON.parse(localStorage.getItem("loneSoldierArr")) || []);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loginData, setLoginData] = useState({});
-    const [submitted, setSubmitted] = useState(false);
-    const [theUser, setTheUser] = useState(null);
-  
-    useEffect(() => {
-      const loginDataString = localStorage.getItem('loginData');
-      if (loginDataString) {
-        const parsedLoginData = JSON.parse(loginDataString);
-        setLoginData(parsedLoginData);
-      }
-    }, []);
-  
-    useEffect(() => {
-      setTheUser(usersArr?.find((user) => user.email === username));
-    }, [usersArr, username]);
-  
-    const handleLogin = (e) => {
-      e.preventDefault();
-  
-      if (theUser && theUser.password === password) {
-        const updatedLoginData = {
-          [username]: password
-        };
-        const updatedLoginDataString = JSON.stringify(updatedLoginData);
-  
-        localStorage.setItem('loginData', updatedLoginDataString);
-  
-        setLoginData(updatedLoginData);
-        setSubmitted(true);
-        // setIsLogIn(true);
-  
-        const url = `/Homepage`;
-        window.history.pushState({}, "", url);
-        window.location.reload();
-      } else {
-        alert("Incorrect username or password. Please try again.");
-      }
-    };
-  
-    return (
-      <div>
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div className="card bg-dark text-white">
-                <div className="card-body p-5 text-center">
-                  <div className="mb-md-5 mt-md-4 pb-5">
-                    <h2 className="fw-bold mb-2 text-uppercase">התחבר בתור חייל</h2>
-                    <p className="text-white-50 mb-5">אנא הכנס אימייל וסיסמה</p>
-                    <div className="form-outline form-white mb-4">
-                      <input type="email"  className="form-control form-control-lg" value={username} onChange={(e) => setUsername(e.target.value)} />
-                      <label className="form-label">אימייל</label>
-                    </div>
-                    <div className="form-outline form-white mb-4">
-                      <input type="password"  className="form-control form-control-lg" value={password} onChange={(e) => setPassword(e.target.value)} />
-                      <label className="form-label">סיסמה</label>
-                    </div>
-                    <button className="btn btn-outline-light btn-lg px-5" type="submit" onClick={handleLogin}>התחבר</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  useEffect(() => {
+    const loginDataString = localStorage.getItem("loginData");
+    if (loginDataString) {
+      const parsedLoginData = JSON.parse(loginDataString);
+      setLoginData(parsedLoginData);
+    }
+  }, []);
+
+  useEffect(() => {
+    setTheUser(usersArr?.find((user) => user.email === username));
+  }, [usersArr, username]);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (theUser && theUser.password === password) {
+      const updatedLoginData = {
+        [username]: password,
+      };
+      const updatedLoginDataString = JSON.stringify(updatedLoginData);
+
+      localStorage.setItem("loginData", updatedLoginDataString);
+
+      setLoginData(updatedLoginData);
+      setSubmitted(true);
+      // setIsLogIn(true);
+
+      const url = `/Homepage`;
+      window.history.pushState({}, "", url);
+      window.location.reload();
+    } else {
+      alert("Incorrect username or password. Please try again.");
+    }
+  };
+
+  return (
+    <div className="logIn-container">
+    
+        <h2 className="login-title  ">התחבר בתור חייל</h2>
+        <div>
+        <div className="form-outline form-white ">
+          <input
+            type="email"
+            className="form-control form-control-lg"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label className="form-label">אימייל</label>
         </div>
+        <div className="form-outline form-white ">
+          <input
+            type="password"
+            className="form-control form-control-lg"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label className="form-label">סיסמה</label>
+        </div>
+        </div>
+        <button
+          className="btn btn-outline-light btn-lg px-5"
+          type="submit"
+          onClick={handleLogin}
+        >
+          התחבר
+        </button>
       </div>
-    );
   
+  );
 }
 
-export default LoginSoldier
+export default LoginSoldier;
