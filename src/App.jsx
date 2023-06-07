9c0a396f20760b3ecbd7192a7729a04af69f66c1
SpeedyQ1 <sahar.y.ash@gmail.com>
2023-06-07 19:21:10 +0300

    resolved changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login'
+import LoginPage from './components/LoginPage'
 import { VolunteerLayout } from './components/VolunteerLayout'
 import { Volunteerpage } from './components/Volunteerpage'
 import { SoldierLayout } from './components/SoldierLayout'
@@ -35,19 +36,21 @@ function App() {
     localStorage.setItem("sweetHomeArr",JSON.stringify(sweetHome.sweetHome));
   }
 
+
+  
   return (
     <>
       <div className='body'>
         <Routes>
-          <Route path="/" element={<FormHouse />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/VolunteerLayout" element={<VolunteerLayout />} />
-          <Route path='/VolunteerLayout/Volunteerpage' element={<Volunteerpage />} />
-          <Route path="/VolunteerLayout/SoldierLayout" element={<SoldierLayout />} />
-          <Route path='/VolunteerLayout/Homepage' element={<Homepage />} />
-          <Route path='/VolunteerLayout/SweetHome' element={<SweetHome />} />
-          <Route path='/VolunteerLayout/WalkingWith' element={<WalkingWith />} />
-          <Route path='/VolunteerLayout/WarmShoulder' element={<WarmShoulder />} />
+          <Route path='/Volunteerpage' element={<Volunteerpage />} />
+          <Route path="/SoldierLayout" element={<SoldierLayout />} />
+          <Route path='/Homepage' element={<Homepage />} />
+          <Route path='/SoldierLayout/SweetHome' element={<SweetHome />} />
+          <Route path='/SoldierLayout/WalkingWith' element={<WalkingWith />} />
+          <Route path='/SoldierLayout/WarmShoulder' element={<WarmShoulder />} />
         </Routes>
       </div>
     </>
