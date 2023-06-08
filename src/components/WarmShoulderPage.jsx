dae4e9832f6900d6d03b4b85d7a2f30185298e18
ChenEylon <cheneylon1@gmail.com>
2023-06-08 09:07:48 +0300

    kdkd

--- src/components/WarmShoulderPage.jsx
+++ src/components/WarmShoulderPage.jsx
@@ -1,6 +1,7 @@
 import React  ,{useState}  from 'react'
 import './WarmShoulder.css'
 import { Link } from "react-router-dom";
+import emailjs from 'emailjs-com';
 
 export const WarmShoulderPage = ({index}) => {
   const [warmShoulderArr, setwarmShoulderArr] = useState(JSON.parse(localStorage.getItem("warmShoulderArr")));
@@ -12,11 +13,22 @@ export const WarmShoulderPage = ({index}) => {
   let  condition = warmShoulderArr[index].condition;
   let  lastName= warmShoulderArr[index]. lastName;
   let name = warmShoulderArr[index].name;
-  let enmail = warmShoulderArr[index].id;
+  let email = warmShoulderArr[index].id;
 
   function handleClick(){
     alert(`תודה שבחרת ב${name} להיות שם בשבילך, הוא יצור קשר בקרוב`)
  }
+ const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_70o4oqk', 'template', e.target, 'ZjMePZlpfng_2A2Jl')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset();
+  };
 
  return (
    <div id="warm-container">
@@ -36,7 +48,21 @@ export const WarmShoulderPage = ({index}) => {
              <h3>מוזמן לפנות אלי בכל שאלה בטלפון: {phone}</h3>
            </div>
            </div>
-         <button className="grab-my-hand-btn" onClick={()=>handleClick()}>grab my hand</button>
+           <form className="email-form" onSubmit={sendEmail}>
+      <input type="hidden" name="id" value={email} />
+        <input type="hidden" name="name" value={name} />
+        <button className="send" type="submit" onClick={handleClick}>
+              <div class="svg-wrapper-1">
+                <div class="svg-wrapper">
+                  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </div>
+              <span>מעוניין</span>
+            </button>
+      </form>
          </div>
          </div>
    </div>
